cbe3a5810fc7822988250f64a5de0c8728e7116a
CHanley-Portfolio <hanley.c@hotmail.com>
2025-07-25 21:11:45 -0300

    Added Frontend Blazor Project

--- Source/VideoLibrary.AppHost/Program.cs
+++ Source/VideoLibrary.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.VideoLibrary_Api>("videolibrary-api");
 
+builder.AddProject<Projects.VideoLibrary>("videolibrary");
+
 builder.Build().Run();
